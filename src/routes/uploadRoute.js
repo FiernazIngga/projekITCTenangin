8b7e8667da6db39b97c45d3e893ceae9f2c413c5
FiernazIngga <inggapratama86@gmail.com>
2025-10-30 21:05:37 +0700

    tes

--- src/routes/uploadRoute.js
+++ src/routes/uploadRoute.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { upProfilePicture } from "../controllers/uploadController.js";
+import { uploadMiddleware, upProfilePicture } from "../controllers/uploadController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/upFotoProfile", verifyToken, upProfilePicture);
+router.post("/upFotoProfile", verifyToken, uploadMiddleware, upProfilePicture);
 
 export default router;
