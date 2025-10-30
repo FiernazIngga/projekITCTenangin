67f65f8a51bd7497248c53103b8d229e92f41f8e
FiernazIngga <inggapratama86@gmail.com>
2025-10-30 12:15:50 +0700

    api

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("<h1>API untuk ITC</h1>");
 });
 
 export default router;
