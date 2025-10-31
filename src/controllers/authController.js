e1e20a7ab927a05a3ff32867ee20d516f53c0d5a
FiernazIngga <inggapratama86@gmail.com>
2025-10-31 18:08:33 +0700

    perbarui

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -13,16 +13,16 @@ import {
 // REGISTER USER
 export const register = async (req, res) => {
     try {
-        const { email, password, name, confirm } = req.body;
+        const { email, password, nama, confirm } = req.body;
         // Cek field kosong
-        if (!email || !password || !name || !confirm) {
+        if (!email || !password || !nama || !confirm) {
             return res.status(400).json({ message: "Semua field wajib diisi" });
         }
         // Cek password dan konfirmasi
         if (password !== confirm) {
             return res.status(400).json({ message: "Password dan konfirmasi password tidak sama" });
         }
-        await registerUser(email, password, name);
+        await registerUser(email, password, nama);
         res.status(201).json({ message: "Registrasi berhasil" });
     } catch (err) {
         res.status(400).json({ message: err.message });
