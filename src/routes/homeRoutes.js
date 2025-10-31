1dd81bc668904b856842b5196025740617b2f3a0
FiernazIngga <inggapratama86@gmail.com>
2025-10-31 13:46:56 +0700

    tes

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { kirimPesanController } from '../controllers/homeController';
+import { kirimPesanController } from '../controllers/homeController.js';
 const router = express.Router();
 
 router.get("/", (req, res) => {
