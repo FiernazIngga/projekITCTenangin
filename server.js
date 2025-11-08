03e3ce78e37d88408aed3b1429cc8239f5fc589c
FiernazIngga <inggapratama86@gmail.com>
2025-11-08 13:57:43 +0700

    ganti port cors

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(cors({
     origin: [
         "http://127.0.0.1:5500",
         "http://localhost:5500",
+        "http://localhost:5173",
         "https://projek-itc-tenangin.vercel.app"
     ],
     credentials: true, 
